be45557cc0ef323da3b8738cafa779c902880226
Gonzalo Barranco Martín <gbarmar753@gmail.com>
2026-04-28 15:21:14 +0200

    Implementación del Panel de Administrador y mejoras de rendimiento

--- frontend/src/services/supabaseClient.js
+++ frontend/src/services/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jpeuehsttjjvkjcwkkrx.supabase.co';          // URL del proyecto
-const supabaseAnonKey = 'sb_publishable_SSNoH9KveUHCAKJsTLTbEg_OdaPInSp';  // Publishable key
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
