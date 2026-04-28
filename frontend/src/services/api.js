46abf70c8e7b1f0c8c7b9f98a9eeff0722f55547
Gonzalo Barranco Martín <gbarmar753@gmail.com>
2026-04-28 20:39:15 +0200

    Actualización de la url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Creamos una instancia de axios configurada
 const api = axios.create({
-    baseURL: 'http://localhost:3000', // La URL de tu backend de Express
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000', // La URL de tu backend de Express
 });
 
 // Este interceptor añadirá el Token automáticamente en cada petición
